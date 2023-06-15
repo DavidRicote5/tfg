7ffdad66958b4187907e0d1fa6d49fd48c824b2d
raul <rauldelarcolombardo@gmail.com>
2023-06-15 10:09:54 +0200

    vers3-definitiva

--- prueba4-front/src/app/pages/user/view-rutinas-user/view-rutinas-user.component.ts
+++ prueba4-front/src/app/pages/user/view-rutinas-user/view-rutinas-user.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit, Pipe} from '@angular/core';
 import { Router } from '@angular/router';
 import { RutinaService } from 'src/app/services/rutina.service';
 import Swal from 'sweetalert2';
@@ -18,15 +18,18 @@ export class ViewRutinasUserComponent implements OnInit {
 
   ngOnInit(): void {
     this.rutinaService.listarRutinas().subscribe(
-      (dato:any) => {
+      (dato: any) => {
         this.rutinas = dato;
       },
       (error) => {
         console.log(error);
         Swal.fire('Error !!','Error al cargar las rutinas','error');
       }
-    )
+    );
   }
+  
+  
+  
 
   actualizarRutina(id:number){
     this.router.navigate(['actualizar-rutina',id]);
@@ -56,5 +59,9 @@ export class ViewRutinasUserComponent implements OnInit {
       }
     })
   }
+
+  
+  
+  
 
 }
