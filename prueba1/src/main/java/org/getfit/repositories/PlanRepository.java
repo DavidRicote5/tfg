7d38f5109fb946b30a2e35cb51bc2e5831e6d451
Rauul-al89 <77743181+Rauul-al89@users.noreply.github.com>
2023-06-16 13:32:07 +0200

    Delete prueba1 directory

--- prueba1/src/main/java/org/getfit/repositories/PlanRepository.java
+++
@@ -1,8 +0,0 @@
-package org.getfit.repositories;
-
-import org.getfit.entities.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-
-}
