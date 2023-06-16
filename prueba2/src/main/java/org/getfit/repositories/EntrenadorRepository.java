e3515d1f8907d3c4bf8ef64b8c0182880cd3b842
Rauul-al89 <77743181+Rauul-al89@users.noreply.github.com>
2023-06-16 13:32:25 +0200

    Delete prueba2 directory

--- prueba2/src/main/java/org/getfit/repositories/EntrenadorRepository.java
+++
@@ -1,8 +0,0 @@
-package org.getfit.repositories;
-
-import org.getfit.entities.Entrenador;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EntrenadorRepository extends JpaRepository<Entrenador, Long> {
-
-}
