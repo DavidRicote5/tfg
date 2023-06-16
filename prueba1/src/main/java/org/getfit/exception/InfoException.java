2669b8aa74b89e19add377f2ca7ff29aff82e445
Rauul-al89 <77743181+Rauul-al89@users.noreply.github.com>
2023-06-16 13:39:50 +0200

    Delete prueba1 directory

--- prueba1/src/main/java/org/getfit/exception/InfoException.java
+++
@@ -1,9 +0,0 @@
-package org.getfit.exception;
-
-public class InfoException extends Exception {
-	private static final long serialVersionUID = -6424354307051530713L;
-
-	public InfoException(String mensaje) {
-		super(mensaje);
-	}
-}
