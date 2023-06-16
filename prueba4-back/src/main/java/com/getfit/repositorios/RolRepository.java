682cc58afc568213024768eb026951b4d965b141
Rauul-al89 <77743181+Rauul-al89@users.noreply.github.com>
2023-06-16 13:36:19 +0200

    Delete prueba4-back directory

--- prueba4-back/src/main/java/com/getfit/repositorios/RolRepository.java
+++
@@ -1,8 +0,0 @@
-package com.getfit.repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.getfit.modelo.Rol;
-
-public interface RolRepository extends JpaRepository<Rol,Long> {
-}
